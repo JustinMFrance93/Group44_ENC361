79dda2c5d71c5591abecb675c242af54ed7a4c52
William Fraser <wfr19@uclive.ac.nz>
2025-03-06 14:37:33 +1300

    addition of basic scheduler to run buttons and led at different frequencies

--- Core/Src/app.c
+++ Core/Src/app.c
@@ -8,43 +8,79 @@
 #include "rgb.h"
 #include "buttons.h"
 
+#define TICK_FREQUENCY_HZ 1000
+#define HZ_TO_TICKS(FREQUENCY_HZ) (TICK_FREQUENCY_HZ/FREQUENCY_HZ)
+
+#define TASK_ONE_FREQUENCY_HZ 2
+#define TASK_TWO_FREQUENCY_HZ 25
+
+#define TASK_ONE_PERIOD_TICKS (TICK_FREQUENCY_HZ/TASK_ONE_FREQUENCY_HZ)
+#define TASK_TWO_PERIOD_TICKS (TICK_FREQUENCY_HZ/TASK_TWO_FREQUENCY_HZ)
+
+static uint32_t taskOneNextRun = 0;
+static uint32_t taskTwoNextRun = 0;
+
+void task_one_execute(void);
+void task_two_execute(void);
+
 int app_main()
 {
 	buttons_init ();
+	taskOneNextRun = HAL_GetTick() + TASK_ONE_PERIOD_TICKS;
+	taskTwoNextRun = HAL_GetTick() + TASK_TWO_PERIOD_TICKS;
+
 	while (1)
 	{
-		HAL_Delay(40);
-		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+		uint32_t ticks = HAL_GetTick();
 
-		if (buttons_checkButton(UP) == PUSHED) //SW1
+		if(ticks > taskOneNextRun)
 		{
-			rgb_colour_all_on();
-			rgb_led_toggle(RGB_UP);
+		  task_one_execute();
+		  taskOneNextRun += TASK_ONE_PERIOD_TICKS;
 		}
 
-
-		if (buttons_checkButton(DOWN) == PUSHED) //SW2
+		if (ticks > taskTwoNextRun)
 		{
-			rgb_colour_all_on();
-			rgb_led_toggle(RGB_DOWN);
-
-		}
-
-		if (buttons_checkButton(RIGHT) == PUSHED) //SW3
-		{
-			rgb_colour_all_on();
-			rgb_led_toggle(RGB_RIGHT);
-
-		}
-
-		if (buttons_checkButton(LEFT) == PUSHED) //SW4
-		{
-			rgb_colour_all_on();
-			rgb_led_toggle(RGB_LEFT);
-
+		  task_two_execute();
+		  taskTwoNextRun += TASK_TWO_PERIOD_TICKS;
 		}
 
 		buttons_update ();
+	}
+}
+
+void task_one_execute(void) //LED
+{
+	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+}
+
+void task_two_execute(void) //Buttons
+{
+	if (buttons_checkButton(UP) == PUSHED) //SW1
+	{
+		rgb_colour_all_on();
+		rgb_led_toggle(RGB_UP);
+	}
+
+
+	if (buttons_checkButton(DOWN) == PUSHED) //SW2
+	{
+		rgb_colour_all_on();
+		rgb_led_toggle(RGB_DOWN);
+
+	}
+
+	if (buttons_checkButton(RIGHT) == PUSHED) //SW3
+	{
+		rgb_colour_all_on();
+		rgb_led_toggle(RGB_RIGHT);
+
+	}
+
+	if (buttons_checkButton(LEFT) == PUSHED) //SW4
+	{
+		rgb_colour_all_on();
+		rgb_led_toggle(RGB_LEFT);
 
 	}
 }
